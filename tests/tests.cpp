776a579e75d07351d3114e5f65af2b9bfba1fde0
Tanmai Raghava <tanmairaghav3836@gmail.com>
2025-03-27 01:51:28 +0530

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,9 +4,21 @@
 #include "../include/dict.h"
 
 TEST_CASE("Inflecting Plurals", "[plural]") {
-    REQUIRE(inflectPlural("mouse") == "mice");
-    REQUIRE(inflectPlural("child") == "children");
-    REQUIRE(inflectPlural("cat") == "cats");
+    // Regular plural forms
+    REQUIRE(inflectPlural("2 cat") == "2 cats");
+    REQUIRE(inflectPlural("3 apple") == "3 apples");
+    REQUIRE(inflectPlural("5 car") == "5 cars");
+
+    // Irregular plural forms
+    REQUIRE(inflectPlural("2 mouse") == "2 mice");
+    REQUIRE(inflectPlural("3 child") == "3 children");
+    REQUIRE(inflectPlural("2 person") == "2 people");
+    REQUIRE(inflectPlural("4 tooth") == "4 teeth");
+    REQUIRE(inflectPlural("3 foot") == "3 feet");
+
+    // Edge cases
+    REQUIRE(inflectPlural("1 sheep") == "1 sheep"); // No plural change
+    REQUIRE(inflectPlural("2 fish") == "2 fish");   // No plural change
 }
 
 TEST_CASE("Inflecting Gender", "[gender]") {
